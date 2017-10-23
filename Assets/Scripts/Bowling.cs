59192e3691552ab0d1b3b5d9db2583f217623beb
Srinivas Annapantula <ssk5492@gmail.com>
2017-10-22 18:02:52 -0700

    Added Scripts
    Scripts to reset bowling ball and pins

---
+++ Assets/Scripts/Bowling.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bowling : MonoBehaviour {
+
+	private Vector3 startposition;
+
+	// Use this for initialization
+	void Start () {
+		startposition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < -5f)
+        {
+            transform.position = startposition;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+	}
+}
---
+++ Assets/Scripts/BowlingPins.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingPins : MonoBehaviour {
+
+    private List<Vector3> startPos = new List<Vector3>();
+    private List<Transform> pins = new List<Transform>();
+
+    private int numPinsDown;
+
+	// Use this for initialization
+	void Start () {
+		foreach(Transform child in transform)
+        {
+            startPos.Add(child.position);
+            pins.Add(child);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (Transform child in pins)
+        {
+            if(child.gameObject.activeInHierarchy && child.position.y < -5f)
+            {
+                child.gameObject.SetActive(false);
+                numPinsDown++;
+            }
+            
+        }
+
+        if(numPinsDown == pins.Count)
+        {
+            Reset();
+        }
+    }
+
+    private void Reset()
+    {
+        for(int i=0; i< pins.Count; i++)
+        {
+            pins[i].gameObject.SetActive(true);
+            pins[i].position = startPos[i];
+            pins[i].rotation = Quaternion.identity;
+            Rigidbody r = pins[i].GetComponent<Rigidbody>();
+            r.velocity = Vector3.zero;
+            r.angularVelocity = Vector3.zero;
+        }
+        numPinsDown = 0;
+    }
+}
